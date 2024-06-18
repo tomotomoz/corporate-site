cd039431b6417fbe837fe62cbdd9bae08027e213
tomotomoz <tomoko.essvivh@gmail.com>
2024-06-18 15:48:27 +0900

    修正

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 'use strict'
 
-gsap.registerPlugin(ScrollTrigger)
+const fade = document.querySelectorAll('.fade')
+// const fade = [...document.querySelectorAll('.fade')];
+console.log(fade)
 
-const elements = gsap.utils.toArray('.fade')
-console.log(elements)
+const setItemActive = (entries) => {
+  entries.forEach((entry) => {
+  console.log(entry)
+  if(entry.isIntersecting){
+    entry.target.classList.add('active')
+  }else{
+    entry.target.classList.remove('active')
+  }
+  })
+}
+const options = {
+rootMargin: '0px',
+threshold: 0.5,
+  }
 
-elements.forEach((el) => {
-  gsap.fromTo(el, {
-  opacity: 0,
-  y: 5,
-  },
-{opacity: 1,
-  y: 0,
-  duration: 0.5,
+const observerElement = new IntersectionObserver(setItemActive, options)
+fade.forEach((fad) => {
+  observerElement.observe(fad)
+}
+)
 
-  scrollTrigger: {
-    trigger: el,
-    start: 'top 70%',
-  // markers: true
-  },
-});
-});
