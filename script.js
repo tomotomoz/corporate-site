8539c65c51928f4cff3bf763570ea8a7e5f80050
tomotomoz <tomoko.essvivh@gmail.com>
2024-07-12 13:27:49 +0900

    修正

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
 'use strict'
 
-// const fade = document.querySelectorAll('.fade')
-// // const fade = [...document.querySelectorAll('.fade')];
-// console.log(fade)
 
-// const setItemActive = (entries) => {
-//   entries.forEach((entry) => {
-//   console.log(entry)
-//   if(entry.isIntersecting){
-//     entry.target.classList.add('active')
-//     observerElement.unobserve(entry.target)
-//   }
-//   })
-// }
-// const options = {
-// rootMargin: '0px',
-// threshold: 0.5,
-//   }
-
-// const observerElement = new IntersectionObserver(setItemActive, options)
-// fade.forEach((fad) => {
-//   observerElement.observe(fad)
-// }
-// )
 document.addEventListener('DOMContentLoaded', () => {
   const options = {
     root: null,
